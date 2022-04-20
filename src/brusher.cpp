b93736a120e8a96dcb0c8c292e2671f34f1e6332
Rohan Dhesikan <rohandhesikan@gmail.com>
2022-04-19 22:57:01 -0700

    fixed brusher

--- src/brusher.cpp
+++ src/brusher.cpp
@@ -1,44 +1,19 @@
 #include "ros/ros.h"
-
-#include <sstream>
 #include <armadillo/brusher_packet.h>
 
-/**
- * This tutorial demonstrates simple sending of messages over the ROS system.
- */
-
-armadillo::brusher_packet* createBrusherPacket(){
-  armadillo::brusher_packet* pkt = new armadillo::brusher_packet();
-  pkt->state = armadillo::brusher_packet::ON;
-  return pkt;
+void brusher_Callback(const armadillo::brusher_packet& msg)
+{
+  ROS_INFO("recevied brusher state: [%d]", msg.state);
 }
-
 
 int main(int argc, char **argv)
 {
-  
   ros::init(argc, argv, "brusher");
-
   ros::NodeHandle n;
 
-  ros::Publisher chatter_pub = n.advertise<armadillo::brusher_packet>("brusher_topic", 1000);
+  ros::Subscriber sub = n.subscribe("brusher_topic", 1000, brusher_Callback);
 
-  ros::Rate loop_rate(10);
-
-  int count = 0;
-  while (ros::ok())
-  {
-
-    armadillo::brusher_packet* pkt = createBrusherPacket();
-
-    chatter_pub.publish(*pkt);
-
-    ros::spinOnce();
-
-    loop_rate.sleep();
-    ++count;
-  }
-
+  ros::spin();
 
   return 0;
 }
