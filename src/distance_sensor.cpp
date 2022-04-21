24072ef7586c688ba24a1baafaebb962a5bb4743
julietfoote22 <juliet.foote22@gmail.com>
2022-04-20 22:55:12 -0700

    added sensor code

--- src/distance_sensor.cpp
+++ src/distance_sensor.cpp
@@ -1,5 +1,6 @@
 #include "ros/ros.h"
 #include "std_msgs/String.h"
+#include <wiringPiI2C.h>
 
 #include <sstream>
 #include <armadillo/distance_sensor_packet.h>
@@ -7,10 +8,16 @@
 /**
  * This tutorial demonstrates simple sending of messages over the ROS system.
  */
+int front_cliff_fd = wiringPiI2CSetup(0x40);
 
 armadillo::distance_sensor_packet* createSensorPacket(){
   armadillo::distance_sensor_packet* pkt = new armadillo::distance_sensor_packet();
-  pkt->front_cliff = .15;
+  int hodist = wiringPiI2CReadReg8(front_cliff_fd,0x5e);
+  int lodist = wiringPiI2CReadReg8(front_cliff_fd,0x5f);
+  int rawdist = (hodist*16+lodist)/4;
+  ROS_INFO("front cliff sensor value: [%d]", msg.rawdist);
+
+ pkt->front_cliff = rawdist;
   return pkt;
 }
 
@@ -25,6 +32,7 @@ int main(int argc, char **argv)
   ros::Publisher chatter_pub = n.advertise<armadillo::distance_sensor_packet>("distance_sensor_topic", 1000);
 
   ros::Rate loop_rate(10);
+
 
   int count = 0;
   while (ros::ok())
