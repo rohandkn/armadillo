8b16533b8ec03f7b019ed07b2cb0519aee145da9
julietfoote22 <juliet.foote22@gmail.com>
2022-04-22 15:05:58 -0700

    added lifter code edits

--- src/lifter.cpp
+++ src/lifter.cpp
@@ -1,11 +1,23 @@
 #include "ros/ros.h"
 #include <armadillo/lifter_packet.h>
+#include <wiringPi.h>
+
+float speed = 0;
+int direction = 0;
 
 void lifter_Callback(const armadillo::lifter_packet& msg)
 {
+  if (msg.speed != speed || msg.direction != direction) {
+    softPwmCreate(0,0,2);
+    softPwmWrite(0,1);
+    digitalWrite(2,1);
+    digitalWrite(3,1);
+  }
+
   ROS_INFO("lifter received dir: [%d]", msg.direction);
   ROS_INFO("lifter received speed: [%f]", msg.speed);
 }
+
 
 int main(int argc, char **argv)
 {
