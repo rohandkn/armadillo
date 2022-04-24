5db8b35dd2b779978490d67bd551fc27805e3e8b
Rohan Dhesikan <rohandhesikan@gmail.com>
2022-04-24 01:54:19 -0700

    added back lifter

--- src/lifter.cpp
+++ src/lifter.cpp
@@ -6,49 +6,21 @@
 
 float speed = 0;
 int direction = 0;
+lifter_obj* front_lifter = new lifter_obj(3, 2, 0);
+lifter_obj* back_lifter = new lifter_obj(7, 6, 5);
 
 void lifter_Callback(const armadillo::lifter_packet& msg) {
-  if (msg.speed != speed || msg.direction != direction) {
-    softPwmStop(0);
-    digitalWrite(2, 1);
-    direction = msg.direction;
-    speed = msg.speed;
-    float scaledSpeed = 25 - (msg.speed/100.0)*23;
-    //checks for hold signal
-    
-    if (msg.direction == armadillo::lifter_packet::HOLD){
-      //sets duty cycle = 100%
-      softPwmWrite(0,scaledSpeed);
-      // enable signal
-      digitalWrite(2,1);
-    }
-    else if (msg.direction == armadillo::lifter_packet::FREE) {
-      // enable signal
-      digitalWrite(2,0);
-    }
-    else {
-      ROS_INFO("set speed to %f", scaledSpeed);
-      softPwmCreate(0,0,scaledSpeed);
-      softPwmWrite(0,scaledSpeed/2);
-      //direction signal
-      digitalWrite(3,msg.direction);
-      // enable signal
-      digitalWrite(2,1);
-     }
-  ROS_INFO("lifter received dir: [%d]", msg.direction);
-  ROS_INFO("lifter received speed: [%f]", msg.speed);
-  }
+  front_lifter->set_speed(msg.direction_front, msg.speed_front);
+  back_lifter->set_speed(msg.direction_back, msg.speed_back);
 }
 
 
 int main(int argc, char **argv)
 {
+
   ros::init(argc, argv, "lifter");
   ros::NodeHandle n;
   wiringPiSetup();
-  pinMode(0, PWM_OUTPUT);
-  pinMode(2, OUTPUT);
-  pinMode(3, OUTPUT);
   ros::Subscriber sub = n.subscribe("lifter_topic", 1000, lifter_Callback);
   ros::spin();
 
