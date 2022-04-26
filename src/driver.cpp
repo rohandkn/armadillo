bd01c97df9114d72aefb01117cf91b13204d6368
Rohan Dhesikan <rohandhesikan@gmail.com>
2022-04-25 21:18:27 -0700

    wrote bot driving function

--- src/driver.cpp
+++ src/driver.cpp
@@ -6,7 +6,11 @@
 float current_speed;
 int enable_pin = 25;
 
-driver_obj* front_left_drv;
+driver_obj* front_left_wheel;
+driver_obj* front_right_wheel;
+driver_obj* back_left_wheel;
+driver_obj* back_right_wheel;
+
 void set_speed(float speed){
   softPwmStop(enable_pin);
       current_speed = speed;
@@ -16,12 +20,38 @@ void set_speed(float speed){
         softPwmWrite(enable_pin,(int) speed);
 }
 
+void set_wheel_dirs(int front_left, int front_right, int back_left, int back_right){
+  front_left_wheel->set_dir(front_left);
+  front_right_wheel->set_dir(front_right);
+  back_left_wheel->set_dir(back_left);
+  back_right_wheel->set_dir(back_right);
+}
+
+void set_bot_dir(int bot_dir){
+  if (bot_dir == armadillo::driver_packet::FORWARD){
+    set_wheel_dirs(driver_obj::ccw, driver_obj::cw, driver_obj::ccw, driver_obj::cw);
+  }
+  else if (bot_dir == armadillo::driver_packet::BACKWARD){
+    set_wheel_dirs(driver_obj::cw, driver_obj::ccw, driver_obj::cw, driver_obj::ccw);
+  }
+  else if (bot_dir == armadillo::driver_packet::LEFT){
+    set_wheel_dirs(driver_obj::cw, driver_obj::cw, driver_obj::ccw, driver_obj::ccw);
+  }
+  else if (bot_dir == armadillo::driver_packet::RIGHT){
+    set_wheel_dirs(driver_obj::ccw, driver_obj::ccw, driver_obj::cw, driver_obj::cw);
+  }
+  else if (bot_dir == armadillo::driver_packet::STOP){
+    set_wheel_dirs(driver_obj::stop, driver_obj::stop, driver_obj::stop, driver_obj::stop);
+  }
+
+}
+
 void driver_Callback(const armadillo::driver_packet& msg)
 {
   softPwmStop(enable_pin);
   //need to pull enable high to set direction otherwise signal interference
   digitalWrite(enable_pin, 1);
-  front_left_drv->set_dir(msg.direction);
+  set_bot_dir(msg.direction);
   //restart speed after direction change
   set_speed(current_speed);
   if (msg.speed != current_speed){
@@ -37,7 +67,10 @@ int main(int argc, char **argv)
   wiringPiSetup();
   pinMode(enable_pin, OUTPUT);
   digitalWrite(enable_pin, 1);
-  front_left_drv = new driver_obj(24, 23);
+  front_left_wheel = new driver_obj(24, 23);
+  back_left_wheel = new driver_obj(24, 23);
+  front_right_wheel = new driver_obj(24, 23);
+  back_right_wheel = new driver_obj(24, 23);
   digitalWrite(enable_pin, 0);
 
   ros::NodeHandle n;
