5f1113cbf1f318e7f61e712c1240ebde035f5bbb
Rohan Dhesikan <rohandhesikan@gmail.com>
2022-05-04 08:14:30 -0400

    basic navigation

--- src/navigation.cpp
+++ src/navigation.cpp
@@ -5,8 +5,11 @@
 #include <armadillo/driver_packet.h>
 #include <armadillo/lifter_packet.h>
 
+ros::Publisher driver_pub;
+ros::Publisher lifter_pub;
+ros::Publisher brusher_pub;
 
-void distance_sensor_Callback(const armadillo::distance_sensor_packet& msg)
+/*void distance_sensor_Callback(const armadillo::distance_sensor_packet& msg)
 {
   ROS_INFO("nav received front cliff sensor: [%f]", msg.front_cliff);
 }
@@ -14,25 +17,35 @@ void distance_sensor_Callback(const armadillo::distance_sensor_packet& msg)
 void encoder_Callback(const armadillo::encoder_packet& msg)
 {
   ROS_INFO("nav received encoder useless information: [%f]", msg.rotations);
-}
+}*/
 
-
-armadillo::brusher_packet* createBrusherPacket(){
+void sendBrusherPacket(int state){
   armadillo::brusher_packet* pkt = new armadillo::brusher_packet();
-  pkt->state = armadillo::brusher_packet::OFF;
-  return pkt;
+  pkt->state = state;
+  brusher_pub.publish(*pkt);
 }
 
-armadillo::driver_packet* createDriverPacket(){
+void sendDriverPacket(int direction, int duration){
   armadillo::driver_packet* pkt = new armadillo::driver_packet();
-  pkt->direction = armadillo::driver_packet::BACKWARD;
-  return pkt;
+  pkt->direction = direction;
+  pkt2->speed = 1.0;
+  driver_pub.publish(*pkt);
+
+  std::this_thread::sleep_for(std::chrono::milliseconds(duration));
+
+  armadillo::driver_packet* pkt2 = new armadillo::driver_packet();
+  pkt2->direction = armadillo::driver_packet::STOP;
+  pkt2->speed = 1.0;
+  driver_pub.publish(*pkt2);
 }
 
-armadillo::lifter_packet* createLifterPacket(){
+void armadillo::lifter_packet* sendLifterPacket(int direction, int module){
   armadillo::lifter_packet* pkt = new armadillo::lifter_packet();
-  pkt->direction =  armadillo::lifter_packet::DOWN;
-  return pkt;
+  pkt->direction =  direction;
+  pkt->module =  module;
+  pkt->speed =  1.0;
+  lifter_pub.publish(*pkt);
+  std::this_thread::sleep_for(std::chrono::milliseconds(5000));
 }
 
 
@@ -41,31 +54,27 @@ int main(int argc, char **argv)
   ros::init(argc, argv, "navigation");
   ros::NodeHandle n;
 
-  ros::Subscriber dist_sub = n.subscribe("distance_sensor_topic", 1000, distance_sensor_Callback);
-  ros::Subscriber enc_sub = n.subscribe("encoder_topic", 1000, encoder_Callback);
-  ros::Publisher brusher_pub = n.advertise<armadillo::brusher_packet>("brusher_topic", 1000);
-  ros::Publisher driver_pub = n.advertise<armadillo::driver_packet>("driver_topic", 1000);
-  ros::Publisher lifter_pub = n.advertise<armadillo::lifter_packet>("lifter_topic", 1000);
+  driver_pub = n.advertise<armadillo::driver_packet>("driver_topic", 1000);
+  lifter_pub = n.advertise<armadillo::lifter_packet>("lifter_topic", 1000);
+  brusher_pub = n.advertise<armadillo::brusher_packet>("brusher_topic", 1000);
 
-  ros::Rate loop_rate(10);
 
-  int count = 0;
-  while (ros::ok())
-  {
+  //ros::Subscriber dist_sub = n.subscribe("distance_sensor_topic", 1000, distance_sensor_Callback);
+  //ros::Subscriber enc_sub = n.subscribe("encoder_topic", 1000, encoder_Callback);
+  //ros::Publisher brusher_pub = n.advertise<armadillo::brusher_packet>("brusher_topic", 1000);
 
-    armadillo::brusher_packet* brusher_pkt = createBrusherPacket();
-    armadillo::lifter_packet* lifter_pkt = createLifterPacket();
-    armadillo::driver_packet* driver_pkt = createDriverPacket();
+  //ros::Rate loop_rate(10);
 
-    brusher_pub.publish(*brusher_pkt);
-    driver_pub.publish(*driver_pkt);
-    //lifter_pub.publish(*lifter_pkt);
+  //int count = 0;
 
-    ros::spinOnce();
+  //armadillo::brusher_packet* brusher_pkt = createBrusherPacket();
+  
+  //lifter_pub.publish(*lifter_pkt);
+  sendBrusherPacket(armadillo::brusher_packet::ON);
+  sendDriverPacket(armadillo::driver_packet::FORWARD, 100);
+  sendLifterPacket(armadillo::lifter_packet::UP, armadillo::lifter_packet::FRONT);
 
-    loop_rate.sleep();
-    ++count;
-  }
+
 
 
 
